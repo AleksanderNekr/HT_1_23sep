b10ab67cacfccca5643ace818d9ca1a4c4657b31
Александр Некрасов <aleksandr_nekrasov@outlook.com>
2021-09-19 10:33:04 +0500

    Добавьте файлы проекта.

---
+++ HT_1_23sep/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT_1_23sep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
